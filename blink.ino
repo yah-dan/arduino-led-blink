419208716e24afdc26b75b376c06a4825ebb7018
yah-dan <daniyanyahaya25@gmail.com>
2026-08-16 12:33:16 +0100

    Create blink.ino

---
+++ blink.ino
@@ -0,0 +1,10 @@
+void setup() {
+  pinMode(13, OUTPUT); // Set pin 13 as an output
+}
+
+void loop() {
+  digitalWrite(13, HIGH); // Turn the LED on
+  delay(1000);            // Wait for 1 second (1000ms)
+  digitalWrite(13, LOW);  // Turn the LED off
+  delay(1000);            // Wait for 1 second
+}
